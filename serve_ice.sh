cc3d26718490b1f75356f0aea1069db41c162dee
Kenneth Larrieu <kennylarrieu@gmail.com>
2023-12-01 11:25:19 -0800

    fix typo

--- serve_ice.sh
+++ serve_ice.sh
@@ -4,5 +4,5 @@ conda activate geospatial
 cd ~/ice_cover_NRT
 # get rid of old nc and raster files (to minimize storage space)
 find ./nc_files/ -mtime +6 -delete
-fine ./rasters/ -mtime +6 -delete
+find ./rasters/ -mtime +6 -delete
 nohup python -u serve_ice.py &> out.log &
